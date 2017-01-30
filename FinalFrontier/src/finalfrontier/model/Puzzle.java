72d9949b70aae305e015fd1058794ced50d62fa7
rschw <rschw@Robs-pc>
2017-01-30 13:22:54 -0700

    1/30 1:20 hashcode, equals, and toString

--- FinalFrontier/src/finalfrontier/model/Puzzle.java
+++ FinalFrontier/src/finalfrontier/model/Puzzle.java
@@ -6,6 +6,7 @@
 package finalfrontier.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -30,6 +31,40 @@ public class Puzzle implements Serializable{
     }
     public void setCorrectAnswer(String CorrectAnswer) {
         this.CorrectAnswer = CorrectAnswer;
+    }
+// hashCode
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + Objects.hashCode(this.Description);
+        hash = 11 * hash + Objects.hashCode(this.CorrectAnswer);
+        return hash;
+    }
+// equals
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Puzzle other = (Puzzle) obj;
+        if (!Objects.equals(this.Description, other.Description)) {
+            return false;
+        }
+        if (!Objects.equals(this.CorrectAnswer, other.CorrectAnswer)) {
+            return false;
+        }
+        return true;
+    }
+// toString
+    @Override
+    public String toString() {
+        return "Puzzle{" + "Description=" + Description + ", CorrectAnswer=" + CorrectAnswer + '}';
     }
     
 }
