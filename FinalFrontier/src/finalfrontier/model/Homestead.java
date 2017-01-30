533a66abc3a3e0b5311329576c7c8501fa73845c
rschw <rschw@Robs-pc>
2017-01-30 12:31:38 -0700

    1/30 12:30 Created getters and setters. Wagon, Map, Location. ToolsAndWeapons, Puzzle, Homestead

--- FinalFrontier/src/finalfrontier/model/Homestead.java
+++ FinalFrontier/src/finalfrontier/model/Homestead.java
@@ -5,10 +5,39 @@
  */
 package finalfrontier.model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author rschw
  */
-public class Homestead {
+public class Homestead implements Serializable{
+    private String Description;
+    private String PurchaseResource;
+    private String SellResource;
+    
+    public Homestead(){
+    }
+// get and set Description
+    public String getDescription() {
+        return Description;
+    }
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+// get and set PurchaseResource
+    public String getPurchaseResource() {
+        return PurchaseResource;
+    }
+    public void setPurchaseResource(String PurchaseResource) {
+        this.PurchaseResource = PurchaseResource;
+    }
+// get and set SellResource
+    public String getSellResource() {
+        return SellResource;
+    }
+    public void setSellResource(String SellResource) {
+        this.SellResource = SellResource;
+    }
     
 }
