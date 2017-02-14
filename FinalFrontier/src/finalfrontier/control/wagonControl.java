b5a88037cb29facedd0cb0013bfc5ad0e73feb9a
Gabriel <Gabriel@10.35.165.113>
2017-02-13 19:03:42 -0700

    Merge origin/master
    Conflicts:
    	FinalFrontier/src/finalfrontier/FinalFrontier.java

--- FinalFrontier/src/finalfrontier/control/wagonControl.java
+++ FinalFrontier/src/finalfrontier/control/wagonControl.java
@@ -13,31 +13,35 @@ import java.util.Scanner;
  * @author rschw
  */
 public class wagonControl implements Serializable {
-    public void calcMaxContent(double length, double width, double height) {
-        String dimension;
+    public int calcMaxContent(String dimension, int width, int height, int increase) {
+               int cost;
         int size;
-    System.out.println("Which dimension are you going to build on?");
-    System.out.println("L = Length");
-    System.out.println("W = Width");
-    System.out.println("H = Height");
-    System.out.println("B = Go back");
-//input dimension
-    Scanner inputDimension = new Scanner(System.in);
-        dimension = inputDimension.next();
+ 
+
                 
-        //if not
-        if (dimension == "L" || dimension == "l") {
-            System.out.println("Constructing on Length of the wagon");
-            System.out.println("How much?");
-        } else if (dimension == "W" || dimension == "w") {
-            System.out.println("Constructing on Width of the wagon");
-            System.out.println("How much?");            
-        }else if (dimension == "H" || dimension == "h") {
-            System.out.println("Constructing on Width of the wagon");
-            System.out.println("How much?");   
-        //}else if (dimension == "B" || dimension == "b") {
-        }else {
-            System.out.println("Invalid input.");
+        if (dimension == null) {
+            return -1;
+        }
+        switch (dimension) {
+            case "L":
+            case "l":
+                 cost =(((width + height)*2)*increase);
+                return cost;
+             
+            case "W":
+            case "w":
+                System.out.println("Constructing on Width of the wagon");
+                System.out.println("How much?");
+                break;
+            case "H":
+            case "h":
+                System.out.println("Constructing on Width of the wagon");
+                System.out.println("How much?");
+                //}else if (dimension == "B" || dimension == "b") {
+                break;
+            default:
+                System.out.println("Invalid input.");
+                break;
         }
 //input size
     Scanner inputSize = new Scanner(System.in);
