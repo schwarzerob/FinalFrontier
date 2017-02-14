e212a652100897f413a9301fe207e732fe6bfd24
Gabriel <Gabriel@10.35.165.113>
2017-02-14 15:32:33 -0700

    Merge origin/master
    Conflicts:
    	FinalFrontier/src/finalfrontier/FinalFrontier.java

---
+++ FinalFrontier/src/finalfrontier/control/wagonControl.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalfrontier.control;
+
+import java.io.Serializable;
+import java.util.Scanner;
+
+/**
+ *
+ * @author rschw
+ */
+public class wagonControl implements Serializable {
+    public int calcMaxContent(String dimension, int width, int height, int increase) {
+               int cost;
+        int size;
+        
+        if (dimension == null) {
+            return -1;
+        }
+        switch (dimension) {
+            case "L":
+            case "l":
+                 cost =(((width + height)*2)*increase);
+                return cost;
+             
+            case "W":
+            case "w":
+                cost =(((width + height)*2)*increase);
+                return cost;
+               
+            case "H":
+            case "h":
+                cost =(((width + height)*2)*increase);
+                return cost;
+                
+            default:
+                System.out.println("Invalid input.");
+                break;
+        }
+//input size
+    Scanner inputSize = new Scanner(System.in);
+        size = inputSize.nextInt();
+        if (size > 10 || size <0) {
+            System.out.println("Invalid Input.");
+        }
+        //calculate area
+        return 0;
+    }
+    
+}
