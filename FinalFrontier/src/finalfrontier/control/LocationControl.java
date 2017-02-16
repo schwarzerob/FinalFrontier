f46d81b31db461790d17b59c870c29e25eca7ad1
rschw <rschw@Robs-pc>
2017-02-16 12:54:20 -0700

    2/16 1:00

--- FinalFrontier/src/finalfrontier/control/LocationControl.java
+++ FinalFrontier/src/finalfrontier/control/LocationControl.java
@@ -20,18 +20,12 @@ the method is complete, the location on the map will
 be changed by the value of either the row, or the column.
 */
 public class LocationControl implements Serializable {
-    public char Location(int row, int column, char visited){
-        char direction = 0;
-        int distance = 0;
+    public int Location(int row, int column, char visited){
+        char direction = 'N';
        row = 1;
        column = 1;
+       int distance = 0;
        visited = '_';
-       
-    Scanner inputDistance = new Scanner(System.in);
-        distance = inputDistance.nextInt();
-        if (distance > 10 || distance <1) {
-            return '_';
-        }
         
         if (direction != 'N' || direction != 'E' || direction != 'S' || direction != 'W') {
             return '_';
@@ -41,19 +35,19 @@ public class LocationControl implements Serializable {
             case 'N':
             case 'n':
                  row = row + distance;
-                 return visited = 'X';
+                 return row;
             case 'E':
             case 'e':
                  column = column - distance;
-                 return visited = 'X';
+                 return column;
             case 'S':
             case 's':
                  row = row - distance;
-                 return visited = 'X';
+                 return row;
             case 'W':
             case 'w':
                  column = column + distance;
-                 return visited = 'X';
+                 return column;
                 
         default:
             System.out.println("Invalid input.");
