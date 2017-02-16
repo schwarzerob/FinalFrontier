ff4959001ae2b552004401034077f8f14857e8c4
rschw <rschw@Robs-pc>
2017-02-16 12:55:37 -0700

    2/16 1:00

--- FinalFrontier/src/finalfrontier/control/wagonControl.java
+++ FinalFrontier/src/finalfrontier/control/wagonControl.java
@@ -53,7 +53,8 @@ public class wagonControl implements Serializable {
     }
 
     //void calcMaxContent(double length, double width, double height) {
-    //    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    //    throw new UnsupportedOperationException("Not supported yet."); 
+    //To change body of generated methods, choose Tools | Templates.
     //}
     
 }
