1a02b9dd5aa1b48c1ce219d3dc301d163b229353
rschw <rschw@Robs-pc>
2017-02-15 21:24:03 -0700

    2/15 9:25p created content to LocationControl

--- FinalFrontier/src/finalfrontier/control/LocationControl.java
+++ FinalFrontier/src/finalfrontier/control/LocationControl.java
@@ -15,50 +15,43 @@ public class LocationControl {
     public char Location(int row, int column, char visited){
         char direction = 0;
         int distance = 0;
-        
-    Scanner inputDirection = new Scanner(System.in);
-        direction = inputDirection.nextInt();
-        if (distance > 10 || distance <0) {
-        }
-        
+       row = 1;
+       column = 1;
+       visited = '_';
     Scanner inputDistance = new Scanner(System.in);
         distance = inputDistance.nextInt();
-        if (distance > 10 || distance <0) {
-            return -1;
+        if (distance > 10 || distance <1) {
+            return '_';
         }
         
         if (direction != 'N' || direction != 'E' || direction != 'S' || direction != 'W') {
-            return '0';
+            return '_';
         }
+            
         switch (direction) {
             case 'N':
             case 'n':
                  row = row + distance;
-                return visited = 'X';
-                
-        switch (direction) 
+                 return visited = 'X';
             case 'E':
             case 'e':
                  column = column - distance;
-                return visited = 'X';
-                
-        switch (direction) 
+                 return visited = 'X';
             case 'S':
             case 's':
                  row = row - distance;
-                return visited = 'X';
-                
-        switch (direction) 
+                 return visited = 'X';
             case 'W':
             case 'w':
                  column = column + distance;
-                return visited = 'X';
+                 return visited = 'X';
                 
         default:
             System.out.println("Invalid input.");
             break;
         }
-        
+    
 //input size
         return 1;    
+    }
 }
