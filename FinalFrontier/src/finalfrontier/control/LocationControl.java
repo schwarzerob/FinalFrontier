0a217350670c75c7db3d68e25338e87de8d4f40b
rschw <rschw@Robs-pc>
2017-02-15 18:47:02 -0700

    2/15 6:45p created content to LocationControl

--- FinalFrontier/src/finalfrontier/control/LocationControl.java
+++ FinalFrontier/src/finalfrontier/control/LocationControl.java
@@ -5,12 +5,60 @@
  */
 package finalfrontier.control;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rschw
  */
 public class LocationControl {
     public char Location(int row, int column, char visited){
-        return 0;
-    }    
+        char direction = 0;
+        int distance = 0;
+        
+    Scanner inputDirection = new Scanner(System.in);
+        direction = inputDirection.nextInt();
+        if (distance > 10 || distance <0) {
+        }
+        
+    Scanner inputDistance = new Scanner(System.in);
+        distance = inputDistance.nextInt();
+        if (distance > 10 || distance <0) {
+            return -1;
+        }
+        
+        if (direction != 'N' || direction != 'E' || direction != 'S' || direction != 'W') {
+            return '0';
+        }
+        switch (direction) {
+            case 'N':
+            case 'n':
+                 row = row + distance;
+                return visited = 'X';
+                
+        switch (direction) 
+            case 'E':
+            case 'e':
+                 column = column - distance;
+                return visited = 'X';
+                
+        switch (direction) 
+            case 'S':
+            case 's':
+                 row = row - distance;
+                return visited = 'X';
+                
+        switch (direction) 
+            case 'W':
+            case 'w':
+                 column = column + distance;
+                return visited = 'X';
+                
+        default:
+            System.out.println("Invalid input.");
+            break;
+        }
+        
+//input size
+        return 1;    
 }
