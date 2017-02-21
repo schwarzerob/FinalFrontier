057e0e60a9d5c831a9b18de13492f53c9ad1af25
rschw <rschw@Robs-pc>
2017-02-21 15:33:33 -0700

    2/21 3:30p GameControl case content

--- FinalFrontier/src/finalfrontier/control/GameControl.java
+++ FinalFrontier/src/finalfrontier/control/GameControl.java
@@ -5,12 +5,88 @@
  */
 package finalfrontier.control;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rschw
  */
 public class GameControl {
     public String NewGame(){
+        
+        /* Very Beginning
+        ‘N’ -New Game
+        ‘C’ -Continue Game
+        ‘L’ -Load Game
+        ‘S’ -Save Game
+        ‘H’ -Help
+        ‘E’ -Exit Game
+        */
+        Scanner doWhat = new Scanner(System.in);
+        char firstChoice = doWhat.next().charAt(0);
+        switch (firstChoice){
+            case 'n':
+            case 'N':
+                //New Game
+                break;
+            case 'c':
+            case 'C':
+                //Continue game currently playing
+                break;
+            case 'l':
+            case 'L':
+                //Load past game
+                break;
+            case 's':
+            case 'S':
+                //Save game
+                break;
+            case 'h':
+            case 'H':
+                //Help list
+                break;
+            case 'e':
+            case 'E':
+                //Exit game
+                break;
+            default: 
+            System.out.println("Invalid input.");
+            break;            
+        }
+        
+        
+        /* The start of each round
+        ‘G’ -Go
+        ‘C’ -Craft
+        ‘I’ -Inventory
+            Show list of resources, and to craft the weapons and tools needed.
+        ‘V’ View the map
+            ‘B’ -go back
+            Player sees the map and their location on it.
+        */
+        firstChoice = doWhat.next().charAt(0);
+        switch (firstChoice){
+            case 'g':
+            case 'G':
+                //Go again
+                break;
+            case 'c':
+            case 'C':
+                //Craft items
+                break;
+            case 'i':
+            case 'I':
+                //Inventory list
+                break;
+            case 'v':
+            case 'V':
+                //View the map
+                break;
+            default: 
+            System.out.println("Invalid input.");
+            break;            
+        }
+        
     return null;
         }
 }
