82c40d0cc8ec0792922ab1da18a57e6e818bf807
rschw <rschw@Robs-pc>
2017-02-24 18:01:08 -0700

    2/24 6:00 pg.45 main menu input success.  continues looping after each choice though.  Example, input "N", triggers the startNewGame function successfully, but immediately prompts for input again.

--- FinalFrontier/src/finalfrontier/view/MainMenuView.java
+++ FinalFrontier/src/finalfrontier/view/MainMenuView.java
@@ -5,14 +5,96 @@
  */
 package finalfrontier.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rschw
  */
 public class MainMenuView {
+        private final String menu;
+        public String value;
+    private String promptMessage;
+    public MainMenuView(){
+        this.menu = "\n"
+                   +"\n-------------------------------------"
+                   +"\n|  Main Menu                        |"
+                   +"\n-------------------------------------"
+                   +"\nN - Start new game"
+                   +"\nG - Get and start saved game"
+                   +"\nH - Het help on how to play the game"
+                   +"\nS - Save game"
+                   +"\nQ - Quit"
+                   +"\n-------------------------------------";
+    }
 
-    void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called ***");
+public void displayMainMenuView() {
+        boolean done = false;
+        do {
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q"))
+                return;
+            done = this.doAction(menuOption);
+            
+        } while (!done);
+    }
+
+    private String getMenuOption() {
+        this.promptMessage = "Please enter you choice from the menu";
+        //String value = "";
+        value = "";
+        Scanner keyboard = new Scanner(System.in);
+        boolean valid = false;
+        while (!valid){
+            System.out.println("\n"+this.menu);
+            value = keyboard.nextLine();
+            value = value.trim();
+            if(value.length() < 1){
+                System.out.println("Invalid value: The value cannot be blank");
+                continue;
+            }
+            break;
+        }
+        return value;
+    }
+
+    public boolean doAction(String menuOption) {
+        value = value.toUpperCase();
+        switch (value){
+            case "N":
+                this.startNewGame();
+                break;
+            case "G":
+                this.startExistingGame();
+                break;
+            case "S":
+                this.saveGame();
+                break;
+            case "H":
+                this.displayHelpMenu();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        
+        return false;
+    }
+
+    private void startNewGame() {
+        System.out.println("*** startNewGame function called ***");
+    }
+
+    private void startExistingGame() {
+        System.out.println("*** StartExistingGame function called ***");
+    }
+
+    private void saveGame() {
+        System.out.println("*** saveGame function called ***");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("*** displayHelpMenu function called ***");
     }
     
 }
