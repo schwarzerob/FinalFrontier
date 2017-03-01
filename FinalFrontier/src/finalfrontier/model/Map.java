a8a46948f19847497c00c555ca09f375ca85b9af
rschw <rschw@Robs-pc>
2017-02-28 17:38:46 -0700

    2/28 5:30pm Progress on LocationMapView, Map.

--- FinalFrontier/src/finalfrontier/model/Map.java
+++ FinalFrontier/src/finalfrontier/model/Map.java
@@ -6,6 +6,7 @@
 package finalfrontier.model;
 
 import java.io.Serializable;
+import finalfrontier.view.LocationMapView;
 
 /**
  *
@@ -13,11 +14,23 @@ import java.io.Serializable;
  */
 public class Map implements Serializable {
     private int row;
-    private int column;
+    private int column = LocationMapView.class.getModifiers();
     char eventChar = '_';
+    
+    //save Location specs to array
+    int mapMatrix[][] = {                
+        {0, 0, 0, 0, 0},              
+        {0, 0, 0, 0, 0},              
+        {0, 0, 0, 0, 0},              
+        {0, 0, 0, 0, 0},              
+        {0, 0, 0, 0, 0},              
+        {0, 0, 0, 0, 0},              
+        {0, 0, 0, 0, 0}
+        };
     
     public Map(){
     }
+    
 // get and set Row
     public int getRow() {
         return row;
@@ -71,36 +84,16 @@ public class Map implements Serializable {
         return "Map{" + "row=" + row + ", column=" + column + '}';
     }
     
-//Display Map
+//Display the Map
     public String displayMap(){
-    System.out.println(" _________________________________________");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|     |     |     |     |     |     |     |");
-    System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
+    System.out.println(" _________");
+    System.out.println("|" + mapMatrix[6][0] + "|" + mapMatrix[6][1] + "|" + mapMatrix[6][2] + "|" + mapMatrix[6][3] + "|" + mapMatrix[0][4] + "|");
+    System.out.println("|" + mapMatrix[5][0] + "|" + mapMatrix[5][1] + "|" + mapMatrix[5][2] + "|" + mapMatrix[5][3] + "|" + mapMatrix[0][4] + "|");
+    System.out.println("|" + mapMatrix[4][0] + "|" + mapMatrix[4][1] + "|" + mapMatrix[4][2] + "|" + mapMatrix[4][3] + "|" + mapMatrix[0][4] + "|");
+    System.out.println("|" + mapMatrix[3][0] + "|" + mapMatrix[3][1] + "|" + mapMatrix[3][2] + "|" + mapMatrix[3][3] + "|" + mapMatrix[0][4] + "|");
+    System.out.println("|" + mapMatrix[2][0] + "|" + mapMatrix[2][1] + "|" + mapMatrix[2][2] + "|" + mapMatrix[2][3] + "|" + mapMatrix[0][4] + "|");
+    System.out.println("|" + mapMatrix[1][0] + "|" + mapMatrix[1][1] + "|" + mapMatrix[1][2] + "|" + mapMatrix[1][3] + "|" + mapMatrix[0][4] + "|");
+    System.out.println("|" + mapMatrix[0][0] + "|" + mapMatrix[0][1] + "|" + mapMatrix[0][2] + "|" + mapMatrix[0][3] + "|" + mapMatrix[0][4] + "|");
         return null;
     }
     
--- FinalFrontier/src/finalfrontier/view/LocationMapView.java
+++ FinalFrontier/src/finalfrontier/view/LocationMapView.java
@@ -14,23 +14,13 @@ import java.util.Scanner;
  */
 public class LocationMapView {
         //variables:
-        int row = 0;
-        int col = 0;
-        int far = 0;
-        //save Location specs to array
-        int mapMatrix[][] = {                
-        {0, 0, 0, 0, 0},              
-        {0, 0, 0, 0, 0},              
-        {0, 0, 0, 0, 0},              
-        {0, 0, 0, 0, 0},              
-        {0, 0, 0, 0, 0},              
-        {0, 0, 0, 0, 0},              
-        {0, 0, 0, 0, 0}
-        };
+        public int row = 6;
+        public int col = 0;
+        public int far = 0;
+        String direction = "";
+        int howFar = 0;
         
         //changes from current location coordinates
-    public LocationMapView() {
-}
     
     //display map
     public String showMap(){
@@ -46,36 +36,54 @@ public class LocationMapView {
     
     //get coordinates for destination
     public String whereToGo(){
-        
-            System.out.println("Which way? N, S, E, or W");
-            String direction = "";
+        while(direction!="N"||direction!="S"||direction!="E"||direction!="W"){    
+            System.out.println("\nWhich way?");  
+            System.out.println(" N ");  
+            System.out.println("W+E");  
+            System.out.println(" S ");
             Scanner keyboard = new Scanner(System.in);
             direction = keyboard.nextLine();
-            System.out.println("Which way? N, S, E, or W");
-            int howFar = 0;
+                System.out.println("\nNot a valid direction");
+                System.out.println("N, S, E, or W");
+        }
+
+            while(direction.length() != 1){
+            System.out.println("\nHow Far?");
+            
             Scanner keyPad = new Scanner(System.in);
             howFar = keyPad.nextInt();
-            if(direction.length() != 1){
-                System.out.println("Invalid input");}
-            
-            if(direction == "N")
-                row = row + howFar;
-            else if(direction == "S")
+            if(direction == "N"){
                 row = row - howFar;
-            else if(direction == "E")
+                if (row <= 0)
+                    System.out.println("Too far!");
+                    }
+            else if(direction == "S"){
+                row = row - howFar;
+                if (row >= 5)
+                    System.out.println("Too far!");
+                    }
+            else if(direction == "E"){
                 col = col + howFar;
-            else if(direction == "W")
+                if (col <= 0)
+                    System.out.println("Too far!");
+                    }
+            else if(direction == "W"){
                 col = col - howFar;
+                if (col >= 5)
+                    System.out.println("Too far!");
+                    }
             else 
-                System.out.println("Invalid input");
-        return direction;        
+                System.out.println("\nInvalid input");
+            }
         //row = Scanner input of direction if N or S
         //col = Scanner input of direction if E or W
         //far = Scanner input of distance row/column + far
+            return null;
     }
     
         //run Location with new row, col, and far
     public boolean doAction(){
+        
         return false;
     }
     
