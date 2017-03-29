8efbbb391f340b58459b8cab60f6db659fe0097b
rschw <rschw@Robs-pc>
2017-03-29 13:43:37 -0600

    3/29 1:45 team 11. Complete through creating logFile path (pg. 10)

--- FinalFrontier/src/finalfrontier/FinalFrontier.java
+++ FinalFrontier/src/finalfrontier/FinalFrontier.java
@@ -9,7 +9,11 @@ import finalfrontier.model.Game;
 import finalfrontier.model.Player;
 import finalfrontier.view.StartProgramView;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
@@ -20,12 +24,13 @@ import java.io.PrintWriter;
 public class FinalFrontier {
     
    public String characterChoice = null;
-   
    private static Game currentGame = null;
    private static Player playerName =null;
    
    private static PrintWriter outFile = null;
    private static BufferedReader inFile = null;
+   
+   private static PrintWriter logFile = null;
     /**
      * @param args the command line arguments
      */
@@ -33,11 +38,61 @@ public class FinalFrontier {
         // create StartProgramViewOrig and display the start program view
         StartProgramView startProgramView = new StartProgramView();
         try{
+            FinalFrontier.inFile = 
+                    new BufferedReader(new InputStreamReader(System.in));
+            FinalFrontier.outFile = new PrintWriter(System.out, true);
+            
+            String filePath = "log.txt";
+            FinalFrontier.logFile = new PrintWriter(filePath);
+            
         startProgramView.display();
         }catch(Throwable te){
-            System.out.println(te.getMessage());
+            System.out.println("Exception" + te.toString() +
+                                "\nCause: " + te.getCause +
+                                "\nMessage" + te.getMessage());
             startProgramView.display();
         }
+        finally {
+            try {
+                if(FinalFrontier.inFile!= null)
+                FinalFrontier.inFile.close();
+                
+                if(FinalFrontier.outFile != null)
+                FinalFrontier.outFile.close();
+                
+                if(FinalFrontier.logFile != null)
+                FinalFrontier.logFile.close();
+            } catch (IOException ex) {
+                System.out.println("Error, closing files");
+                return;
+            }
+        }
      }
+
+    public static PrintWriter getOutFile() {
+        return outFile;
+    }
+
+    public static void setOutFile(PrintWriter outFile) {
+        FinalFrontier.outFile = outFile;
+    }
+
+    public static BufferedReader getInFile() {
+        return inFile;
+    }
+
+    public static void setInFile(BufferedReader inFile) {
+        FinalFrontier.inFile = inFile;
+    }
+
+    public static PrintWriter getLogFile() {
+        return logFile;
+    }
+
+    public static void setLogFile(PrintWriter logFile) {
+        FinalFrontier.logFile = logFile;
+    }
+    
+    
 }
 
---
+++ FinalFrontier/src/finalfrontier/view/ErrorView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalfrontier.view;
+
+import finalfrontier.FinalFrontier;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author rschw
+ */
+public class ErrorView {
+    private static final PrintWriter errorFile = FinalFrontier.getOutFile();
+    
+    public static void display(String className, String errorMessage){
+        errorFile.println(
+            "------------------" +
+            "\n-Error-"+ errorMessage +
+            "\n------------------");
+    }
+}
