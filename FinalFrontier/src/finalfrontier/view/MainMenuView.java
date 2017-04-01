2a2417268ee12c92efab738dc4b0815b89300c4a
rschw <rschw@Robs-pc>
2017-04-01 17:11:23 -0600

    4/1 5:00pm Attempt for Individual Assignment.

--- FinalFrontier/src/finalfrontier/view/MainMenuView.java
+++ FinalFrontier/src/finalfrontier/view/MainMenuView.java
@@ -8,8 +8,14 @@ package finalfrontier.view;
 import finalfrontier.FinalFrontier;
 import finalfrontier.control.CharacterControl;
 import finalfrontier.control.GameControl;
+import finalfrontier.exceptions.GameControlException;
+import finalfrontier.exceptions.MainMenuException;
 import finalfrontier.model.Player;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,41 +27,49 @@ public class MainMenuView extends View{
                    +"\n-------------------------------------"
                    +"\n|   Main Menu                       |"
                    +"\n-------------------------------------"
-                   +"\nN - Start new game"
+                   +"\nN - New game"
+                   +"\nS - Save game"
                    +"\nL - Load game"
                    +"\nH - Get help on how to play the game"
-                   +"\nS - Save game"
+                   +"\nP - Print reports"
                    +"\nQ - Quit"
                    +"\n-------------------------------------");
     }
 @Override
     public boolean doAction(String value) {
-        value = value.toUpperCase();
-        switch (value){
-            case "N":
-                this.startNewGame();
-                break;
-            case "L":
-                this.loadGame();
-                break;
-            case "S":
-                this.saveGame();
-                break;
-            case "H":
-                this.displayHelpMenu();
-                break;
-            default:
-                this.console.println("\n*** Invalid selection ***"
-                                  +"\n    Try again");
-                break;
+        try {
+            value = value.toUpperCase();
+            switch (value){
+                case "N":
+                    this.startNewGame();
+                    break;
+                case "S":
+                    this.saveGame();
+                    break;
+                case "L":
+                    this.loadGame();
+                    break;
+                case "H":
+                    this.displayHelpMenu();
+                    break;
+                case "P":
+                    this.printReports();
+                    break;
+                default:
+                    this.console.println("\n*** Invalid selection ***"
+                            +"\n    Try again");
+                    break;
+            }
+            
+            return false;
+        } catch (MainMenuException ex) {
+            Logger.getLogger(MainMenuView.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        return false;
+        return true;
     }
 
     private void startNewGame() {
         
-        StartProgramView playerName = new StartProgramView();
         GameMenuView gameMenu = new GameMenuView();
         gameMenu.display();
     }
@@ -76,8 +90,22 @@ public class MainMenuView extends View{
 
     private void displayHelpMenu() {
         HelpMenuView helpMenuView = new HelpMenuView();
-        
         helpMenuView.display();
+    }
+
+    private void printReports() 
+            throws MainMenuException {
+        this.console.println("*** printReports function called ***");
+        this.console.println("\n\nEnter the file path for the report to be saved.");
+        String filePath = null;
+        Object report = null;
+        try(FileOutputStream fops = new FileOutputStream(filePath)){
+            ObjectOutputStream output = new ObjectOutputStream(fops);
+            output.writeObject(report);
+        }catch(Exception ex){
+            throw new MainMenuException(ex.getMessage());
+        }
+        
     }
     
 }
